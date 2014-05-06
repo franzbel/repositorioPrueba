08358f8e0b6817700b985e1f62353bbed51dd3a4
Franz Beltran <franzbeltran79@hotmail.com>
2014-05-05 20:40:22 -0700

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Casasmart::Application.config.secret_key_base = '4b6aaa3fd4ab3b60eb436db364fa6702213ce6e40a4eac4ebf7aebf72687148d9be1c22a75f722dc50996dc6f36c6fefbc8c90f4989d4d2adb002be1a1084be7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Casasmart::Application.config.session_store :cookie_store, key: '_casasmart_session'
